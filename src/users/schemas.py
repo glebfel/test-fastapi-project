4da7955897109af3ea9c58ffd78cc6aa0f21536e
glebfel <felyust@list.ru>
2023-08-25 20:09:08 +0300

    add auto conversion for user models: UpdateUserInfo -> UserInfo

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,18 +1,30 @@
+from __future__ import annotations
+
 from datetime import datetime
 
 from pydantic import BaseModel, Field
 
-
-class UserInfo(BaseModel):
-    user_id: int
-    first_name: str
-    last_name: str
-    username: str
-    email: str
-    registered_at: datetime
+from src.users.models import User
 
 
 class UpdateUserInfo(BaseModel):
     first_name: str = Field(max_length=30, default=None)
     last_name: str = Field(max_length=30, default=None)
     username: str = Field(max_length=30, default=None)
+
+
+class UserInfo(UpdateUserInfo):
+    user_id: int
+    email: str
+    registered_at: datetime
+
+    @classmethod
+    def marshal(cls, model: User) -> UserInfo:
+        return cls(
+            user_id=model.user_id,
+            first_name=model.first_name,
+            last_name=model.last_name,
+            username=model.username,
+            email=model.email,
+            registered_at=model.registered_at,
+        )
