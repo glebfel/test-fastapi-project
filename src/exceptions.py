134c5eb514972988b2111e739af3de0f4fca1c31
glebfel <felyust@list.ru>
2023-08-25 18:23:38 +0300

    add global exceptions module

---
+++ src/exceptions.py
@@ -0,0 +1,13 @@
+class BaseAPIException(Exception):
+    """Base class for all parser errors"""
+
+    def __str__(self):
+        return "Something goes wrong with API-service"
+
+
+class DatabaseElementNotFoundError(BaseAPIException):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
