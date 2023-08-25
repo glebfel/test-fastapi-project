eed44bd0fb00bad2fd020ec0cd5374bd7248cdf2
glebfel <felyust@list.ru>
2023-08-25 20:08:15 +0300

    add necessary crud methods for users table

--- src/users/crud.py
+++ src/users/crud.py
@@ -10,6 +10,20 @@ def get_user_by_email(email: str) -> User:
         return user
 
 
+def get_user_by_id(user_id: int) -> User:
+    with get_db() as session:
+        if not (user := session.query(User).filter_by(user_id=user_id).first()):
+            raise DatabaseElementNotFoundError(f"User with id '{user_id}' not found")
+        return user
+
+
+def get_users_by_firstname(firstname: str) -> list[User]:
+    with get_db() as session:
+        if not (user := session.query(User).filter_by(firstname=firstname).all()):
+            raise DatabaseElementNotFoundError(f"Users with firstname '{firstname}' not found")
+        return user
+
+
 def get_all_users() -> list[User]:
     with get_db() as session:
         return session.query(User).all()
@@ -27,7 +41,13 @@ def add_new_user(first_name: str, last_name, email: str, username: str, hashed_p
     return user
 
 
-def update_user_by_email(email: str, **kwargs):
+def update_user_by_id(user_id: int, **kwargs):
     with get_db() as session:
-        session.query(User).filter_by(email=email).update(kwargs)
+        session.query(User).filter_by(user_id=user_id).update(kwargs)
+        session.commit()
+
+
+def delete_user_by_id(user_id: int):
+    with get_db() as session:
+        session.query(User).filter_by(user_id=user_id).delete()
         session.commit()
