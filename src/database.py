5f3df44e1eee195dd51b9f69775de6336cab6772
glebfel <felyust@list.ru>
2023-08-26 17:17:08 +0300

    change db name

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-DATABASE_URL = 'sqlite+aiosqlite:///./test.db'
+DATABASE_URL = 'sqlite+aiosqlite:///./users.db'
 
 engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
