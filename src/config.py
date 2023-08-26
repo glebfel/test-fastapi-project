6f7e58b3adc7200d3f21c70e93629081051c4fc5
glebfel <felyust@list.ru>
2023-08-26 20:42:09 +0300

    made config read env not defined in settings object

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from pydantic import Field
+from pydantic import Extra, Field
 from pydantic_settings import BaseSettings
 
 
@@ -14,6 +14,7 @@ class Settings(BaseSettings):
         case_sentive = False
         env_file = '.env'
         env_file_encoding = 'utf-8'
+        extra = Extra.allow
 
 
 # load env from file
